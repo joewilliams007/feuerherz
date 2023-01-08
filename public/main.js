58bfbb415856722166a0364ca91e8d2e12d73f82
Universe <williamsthehack@gmail.com>
2023-01-08 17:50:10 +0100

    up

--- public/main.js
+++ public/main.js
@@ -75,6 +75,8 @@ function postChat() {
         return alert("enter message")
     }
 
+    setCookie("username", username, 30);
+
     console.log("sending chat"+username+" "+message)
 
     document.getElementById('message').value = ""
@@ -112,4 +114,36 @@ function getChat() {
         document.getElementById('chat').innerHTML = response.chat
     })
     .catch(err => console.log(err))
+}
+
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+function getCookie(cname) {
+let name = cname + "=";
+let decodedCookie = decodeURIComponent(document.cookie);
+let ca = decodedCookie.split(';');
+for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+    c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+    return c.substring(name.length, c.length);
+    }
+}
+return "";
+}
+
+function checkCookie() {
+let user = getCookie("username");
+if (user != "") {
+    document.getElementById('username').value = user
+} else {
+
+}
 }
